bdd965edd9185bcef0f04561f8e090cd13b4a58a
kalpaabeysiri <kalpamadhushan2001@gmail.com>
2026-01-17 00:02:45 +0530

    initial commit

---
+++ src/main/java/lk/neo369/kamakade/KamakadeApplication.java
@@ -0,0 +1,13 @@
+package lk.neo369.kamakade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamakadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamakadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/neo369/kamakade/KamakadeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.neo369.kamakade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamakadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
